e952426f98d9ad053aff89e7e44c347646afd00f
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-10-28 15:28:21 -0300

    Create DbContext

---
+++ Dima.Api/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Dima.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Dima.Api.Data
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
