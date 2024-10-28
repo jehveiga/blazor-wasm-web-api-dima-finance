bf0af4acaf3bf90b071402699374146fc54bb9f8
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-10-28 17:09:52 -0300

    Cria a classe de Request

---
+++ Dima.Core/Requests/PagedRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Dima.Core.Requests
+{
+    public abstract class PagedRequest : Request
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 25;
+    }
+}
