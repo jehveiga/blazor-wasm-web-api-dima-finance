be94187c27f6debe88b631c319c4c867a270304b
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-10-28 12:08:09 -0300

    Cria a classe Request

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -17,9 +17,24 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
-app.MapGet("/v1/transactions", () =>
+app.MapGet(pattern: "/v1/transactions", handler: () =>
 {
 
 });
 
 app.Run();
+
+// Request
+public class Request
+{
+    public string Title { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public int Type { get; set; }
+    public decimal Amount { get; set; }
+    public long CategoryId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+}
+
+// Response
+
+// Handler
