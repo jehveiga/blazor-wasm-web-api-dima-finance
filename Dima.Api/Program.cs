687e0803f3ae89a03459d0f19ec0ed366a6ce038
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-11-04 12:36:17 -0300

    Adiciona ao container de serviços o serviço do Identity

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,6 +1,7 @@
 using Dima.Api.Data;
 using Dima.Api.Endpoints;
 using Dima.Api.Handlers;
+using Dima.Api.Models;
 using Dima.Core.Handlers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -21,6 +22,12 @@ builder.Services.AddAuthorization();
 string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+// Adicionando o serviço para trabalhar com identity
+builder.Services.AddIdentityCore<User>() // Adiciona ao serviço do Identity a classe customizada que foi criada de User
+                .AddRoles<IdentityRole<long>>() // Adiciona ao serviço do Identity a classe customizada que foi criada de Role
+                .AddEntityFrameworkStores<AppDbContext>() // Adicionando o serviço do banco que será usado para criação das tabelas do Identity
+                .AddApiEndpoints(); // Cria automaticamente os endpoint para o serviço de autenticação
 
 builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
 builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
