8e4285e3e9a3a861184236165cb6336d8db2a67c
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-11-08 09:31:32 -0300

    Impementa a configuração do MudBlazor

--- Dima.Web/Program.cs
+++ Dima.Web/Program.cs
@@ -1,11 +1,13 @@
+using Dima.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Dima.Web;
+using MudBlazor.Services;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
